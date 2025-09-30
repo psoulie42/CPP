f281aaf7ed2bd78dacab3fec65652c55ce02440f
Achille Soulié <psoulie42gmail.com>
2025-09-30 16:46:59 +0200

    holy shit im late af

---
+++ 01/ex05/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include "class.Harl.hpp"
+
+int	main()
+{
+	Harl harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	harl.complain("hello");
+}
